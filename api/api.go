0e8ce59e49847e7f7b7b41225f3b9e11625acce9
memerelics <takuya21hashimoto@gmail.com>
2014-08-15 18:41:45 +0900

    API Get/Post with private fire function

--- api/api.go
+++ api/api.go
@@ -4,9 +4,18 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"../config"
 	"../utils"
+)
+
+const (
+	GetBase   = "https://app.asana.com"
+	PostBase  = "https://app.asana.com/api/1.0"
+	UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) " +
+		"AppleWebKit/537.36 (KHTML, like Gecko) " +
+		"Chrome/36.0.1985.125 Safari/537.36"
 )
 
 type Base struct {
@@ -15,24 +24,33 @@ type Base struct {
 }
 
 func Get(path string, params url.Values) []byte {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", buildGetUrl("https://app.asana.com", path, params), nil)
-	ua := "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
-	req.Header.Set("User-Agent", ua)
-	req.SetBasicAuth(config.Load().Api_key, "")
-	resp, err := client.Do(req)
+	var getURL string
+	if params == nil || params.Encode() == "" {
+		getURL = GetBase + path
+	} else {
+		getURL = GetBase + path + "?" + params.Encode()
+	}
+	req, err := http.NewRequest("GET", getURL, nil)
 	utils.Check(err)
-
-	contents, err2 := ioutil.ReadAll(resp.Body)
-	utils.Check(err2)
-
-	return contents
+	return fire(req)
 }
 
-func buildGetUrl(host string, path string, params url.Values) string {
-	if params == nil || params.Encode() == "" {
-		return host + path
-	} else {
-		return host + path + "?" + params.Encode()
-	}
+func Post(path string, data string) []byte {
+	req, err := http.NewRequest("POST", PostBase+path, strings.NewReader(data))
+	utils.Check(err)
+	return fire(req)
+}
+
+func fire(req *http.Request) []byte {
+	client := &http.Client{}
+
+	req.Header.Set("User-Agent", UserAgent)
+	req.SetBasicAuth(config.Load().Api_key, "")
+
+	resp, err := client.Do(req)
+	body, err := ioutil.ReadAll(resp.Body)
+
+	utils.Check(err)
+
+	return body
 }
