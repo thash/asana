60b9d3a072809f899b7b7dffd3c46524e8b7665c
memerelics <takuya21hashimoto@gmail.com>
2016-01-16 04:54:24 +0900

    fix asana_test

--- asana_test.go
+++ asana_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestMain(t *testing.T) {
 	expects := []string{"config", "workspaces", "tasks", "task",
-                        "comment", "done", "due"}
+                        "comment", "done", "due", "browse"}
 	cmds := defs()
 	if len(cmds) != len(expects) {
 		t.Error("commands mismatch")
